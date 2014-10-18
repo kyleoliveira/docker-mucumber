0bb63d6d664a9df26804dd5abfa6a72cab5f4d07
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-10-17 20:03:24 -0400

    Almost there.

--- bin/install_gems.sh
+++ bin/install_gems.sh
@@ -11,6 +11,9 @@ perl -ane 'system("gem install " . @F[0])'
 # Next, we'll install gems required by the Gemfile
 cd /cucumber
 if [ -e "Gemfile" ]; then
+  # Fix the kuality-kfs line, if necessary
+  sed "s/kuality-kfs.*git /kuality-kfs\', :git/g" Gemfile > tmp.gf && mv tmp.gf Gemfile
+
   # Gemfile provided
   bundle install  
 else
